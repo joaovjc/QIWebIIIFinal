b40f6ac90f509e0a1f89119c51aec6738e11d3f2
Your Name <you@example.com>
2021-10-20 16:19:49 -0300

    front: home e começo da implementação do thymeleaf | backend: começo do modelo do banco com spring data

---
+++ rpg_site/src/main/java/br/com/joaovjc/rpg_site/model/Item.java
@@ -0,0 +1,44 @@
+package br.com.joaovjc.rpg_site.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "itens")
+public class Item {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String nome;
+	private BigDecimal peso;
+	private String descricao;
+	@Enumerated(EnumType.STRING)
+	private TipoItem tipoItem;
+	
+	public Item(String nome, BigDecimal peso, String descricao) {
+		this.nome = nome;
+		this.peso = peso;
+		this.descricao = descricao;
+	}
+	
+	public Item() {}
+	public String getNome() {return nome;}
+	public void setNome(String nome) {this.nome = nome;}
+	public BigDecimal getPeso() {return peso;}
+	public void setPeso(BigDecimal peso) {this.peso = peso;}
+	public String getDescricao() {return descricao;}
+	public void setDescricao(String descricao) {this.descricao = descricao;}
+	public long getId() {return id;}
+	public TipoItem getTipoItem() {return tipoItem;}
+	public void setTipoItem(TipoItem tipoItem) {this.tipoItem = tipoItem;}
+	
+	
+}
---
+++ rpg_site/src/main/java/br/com/joaovjc/rpg_site/model/TipoItem.java
@@ -0,0 +1,9 @@
+package br.com.joaovjc.rpg_site.model;
+
+public enum TipoItem {
+	
+	DINHEIRO,
+	EQUIPAVEL_MAO,
+	ARMADURA;
+	
+}
