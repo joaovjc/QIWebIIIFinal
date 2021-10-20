543f8dc1883cc74ace910461dded29657161bdb1
Your Name <you@example.com>
2021-10-20 12:59:22 -0300

    front pronto: inventario, formReg, formPers, login

---
+++ rpg_site/src/main/java/br/com/joaovjc/rpg_site/RpgSiteApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaovjc.rpg_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgSiteApplication.class, args);
+	}
+
+}
---
+++ rpg_site/src/test/java/br/com/joaovjc/rpg_site/RpgSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaovjc.rpg_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
